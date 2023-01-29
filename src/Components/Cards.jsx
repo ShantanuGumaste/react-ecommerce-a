172c615436d9664a17e8a8325344e74c23e24c52
Shantanu Gumaste <shantanugumaste88@gmail.com>
2023-01-29 15:33:06 +0530

    card shadows

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -7,9 +7,9 @@ export default function Cards() {
       <div className="best_selling text-center">
         <h3>Our Best Selling Sports Collection</h3>
       </div>
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mx-3">
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p2401133/k$ca8b46049f035747f7c2006850aff98a/men-s-fitness-standard-breathable-jacket-green.jpg?format=auto&quality=70&f=300x0"
               className="card-img-top"
@@ -24,7 +24,7 @@ export default function Cards() {
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p2155519/675a901c012338809f9e6dda7dd6ea6b/p2155519.jpg?format=auto&quality=70&f=650x0"
               className="card-img-top"
@@ -37,7 +37,7 @@ export default function Cards() {
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p2172599/31e38371875cbaa821ecf41aaf551f63/p2172599.jpg?format=auto&quality=70&f=1024x0"
               className="card-img-top"
@@ -46,14 +46,13 @@ export default function Cards() {
             <div className="card-body">
               <h5 className="card-title">QUECHUA</h5>
               <p className="card-text">
-                Men’s Waterproof Winter Hiking Jacket - SH100 Warm -5°C
-                Waterproof - Blue
+                Men’s Waterproof Winter Hiking Jacket - Waterproof - Blue
               </p>
             </div>
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p1698332/62c038f4c19161ffac6c587d1d17d566/p1698332.jpg?format=auto&quality=70&f=1024x0"
               className="card-img-top"
@@ -68,7 +67,7 @@ export default function Cards() {
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p2155519/675a901c012338809f9e6dda7dd6ea6b/p2155519.jpg?format=auto&quality=70&f=650x0"
               className="card-img-top"
@@ -81,7 +80,7 @@ export default function Cards() {
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p2401133/k$ca8b46049f035747f7c2006850aff98a/men-s-fitness-standard-breathable-jacket-green.jpg?format=auto&quality=70&f=300x0"
               className="card-img-top"
@@ -96,7 +95,7 @@ export default function Cards() {
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p1698332/62c038f4c19161ffac6c587d1d17d566/p1698332.jpg?format=auto&quality=70&f=1024x0"
               className="card-img-top"
@@ -111,7 +110,7 @@ export default function Cards() {
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card shadow">
             <img
               src="https://contents.mediadecathlon.com/p2172599/31e38371875cbaa821ecf41aaf551f63/p2172599.jpg?format=auto&quality=70&f=1024x0"
               className="card-img-top"
@@ -120,8 +119,7 @@ export default function Cards() {
             <div className="card-body">
               <h5 className="card-title">QUECHUA</h5>
               <p className="card-text">
-                Men’s Waterproof Winter Hiking Jacket - SH100 Warm -5°C
-                Waterproof - Blue
+                Men’s Waterproof Winter Hiking Jacket - Waterproof - Blue
               </p>
             </div>
           </div>
